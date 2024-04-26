4ca18e2d03a9b63b0cdca133fdfb848ed961ee1a
Jehuda Moshkovich <jehuda@Jehudas-MacBook-Air.local>
2024-04-26 16:19:02 +0100

    tera

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+    region = "eu-west-2"  
+}
+
+
+resource "aws_security_group" "webtraffic" {
+name = "Allow HTTPS"
+
+
+dynamic "ingress" {
+  for_each = [80,443]
+  iterator = port
+  
+  content {
+ 
+  from_port = port.value
+  to_port = port.value
+  protocol = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+dynamic "egress" {
+  for_each = [25, 80,443, 8080]
+  iterator = port
+  
+  content {
+ 
+  from_port = port.value
+  to_port = port.value
+  protocol = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+}
+
+
+resource "aws_instance" "ec2" {
+  ami = "ami-019a292cfb114a776"
+instance_type = "t2.micro"
+security_groups = [aws_security_group.webtraffic.name]
+}
